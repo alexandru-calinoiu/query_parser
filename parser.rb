9aae780979a4764b5b396bf79e1e016241460da2
Calin <calinoiu.alexandru@agilefreaks.com>
2017-07-03 16:05:30 +0300

    Add clause parsing

--- parser.rb
+++ parser.rb
@@ -11,45 +11,91 @@ class MyParser < Parslet::Parser
 	root(:query)
 end
 
-pp MyParser.new.parse('hello world')
+MyParser.new.parse('hello world')
 
 begin
 	MyParser.new.parse('hello, world')
 rescue Parslet::ParseFailed => e
-	pp e.parse_failure_cause.ascii_tree
+	e.parse_failure_cause.ascii_tree
 end
 
 class QueryParser < Parslet::Parser
 	rule(:term) { match('[^\s]').repeat(1).as(:term) }
+	rule(:operator) { (str('+') | str('-')).as(:operator) }
+	rule(:clause) { (operator.maybe >> term).as(:clause) }
 	rule(:space) { match('\s').repeat(1) }
-	rule(:query) { (term >> space.maybe).repeat.as(:query) }
+	rule(:query) { (clause >> space.maybe).repeat.as(:query) }
 	root(:query)
 end
 
+class Clause
+	attr_reader :operator, :term
+
+	def initialize(operator, term)
+		@operator = Operator.symbol(operator)
+		@term = term
+	end
+end
+
+class Operator
+	def self.symbol(str)
+		case str
+		when '+'
+			:must
+		when '-'
+			:must_not
+		when nil
+			:should
+		else
+			raise "Unknown operator: #{str}"
+		end
+	end
+end
+
 class QueryTransformer < Parslet::Transform
-	rule(term: simple(:term)) { term.to_s }
-	rule(query: sequence(:terms)) { Query.new(terms) }
+	rule(clause: subtree(:clause)) do
+		Clause.new(clause[:operator]&.to_s, clause[:term].to_s)
+	end
+	rule(query: sequence(:clauses)) { Query.new(clauses) }
 end
 
 class Query
-	def initialize(terms)
-		@terms = terms
+	def initialize(clauses)
+		grouped = clauses.chunk { |c| c.operator }.to_h
+		@should_terms = grouped.fetch(:should, []).map(&:term)
+		@must_not_terms = grouped.fetch(:must_not, []).map(&:term)
+		@must_terms = grouped.fetch(:must, []).map(&:term)
 	end
 
 	def to_elasticsearch
-		{
+		query = {
 			query: {
-				match: {
-					title: {
-						query: @terms.join(' '),
-						operator: 'or'
-					}
+				bool: {
 				}
 			}
 		}
+
+		bool = query[:query][:bool]
+		bool[:should] = @should_terms.map { |t| match(t) } if @should_terms.any?
+		bool[:must] = @must_terms.map { |t| match(t) } if @must_terms.any?
+		bool[:must_not] = @must_not_terms.map { |t| match(t) } if @must_not_terms.any?
+
+		query
 	end
+
+	private
+
+		def match(term)
+			{
+				match: {
+					title: {
+						query: term
+					}
+				}
+			}
+		end
 end
 
-parse_tree = QueryParser.new.parse('cat in the hat')
+parse_tree = QueryParser.new.parse('the +cat in the -hat')
 query = QueryTransformer.new.apply(parse_tree)
 pp query.to_elasticsearch
